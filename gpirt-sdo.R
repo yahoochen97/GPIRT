6a7876d7c854bdb2eee1d11eaa1bae4b78131f86
Yehu Chen <chenyehu@wustl.edu>
2022-03-16 17:44:59 -0500

    -2 to 2

--- gpirt-sdo.R
+++ gpirt-sdo.R
@@ -37,8 +37,8 @@ HYP = paste(TYPE, "_C_", C, '_n_', n, '_m_', m, '_SEED_', SEED, sep="")
 load(file=paste("./data/", HYP, ".RData" , sep=""))
 set.seed(SEED)
 
-SAMPLE_ITERS = 100
-BURNOUT_ITERS = 100
+SAMPLE_ITERS = 200
+BURNOUT_ITERS = 200
 THIN = 1
 beta_prior_sds =  matrix(0.5, nrow = 2, ncol = ncol(data_train))
 samples <- gpirtMCMC(data_train, SAMPLE_ITERS,BURNOUT_ITERS, THIN,
