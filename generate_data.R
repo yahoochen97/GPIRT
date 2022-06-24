7059ec557f1e2b909a98f9add5fa4a46fa6c8fd0
Yehu Chen <chenyehu@wustl.edu>
2022-06-24 13:07:12 -0500

    sample 100 iters

--- generate_data.R
+++ generate_data.R
@@ -2,13 +2,13 @@
 args = commandArgs(trailingOnly=TRUE)
 options(show.error.locations = TRUE)
 
-# gpirt_path = "~/Documents/Github/gpirt"
-# setwd(gpirt_path)
-# setwd("../OrdGPIRT")
+gpirt_path = "~/Documents/Github/gpirt"
+setwd(gpirt_path)
+setwd("../OrdGPIRT")
 
 if (length(args)==0) {
-  SEED = 1
-  C = 5
+  SEED = 10
+  C = 2
   n = 1000
   m = 100
   horizon = 10
