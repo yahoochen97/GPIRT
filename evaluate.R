9d77a09efe65149025c05213d596961ed263a187
Yehu Chen <chenyehu@wustl.edu>
2022-07-11 13:38:55 -0500

    compare

--- evaluate.R
+++ evaluate.R
@@ -33,15 +33,15 @@ for(i in 1:length(MODELS)){
   for (h in 1:horizon) {
     cor_theta = c(cor_theta, cor(theta[,h], pred_theta[,h]))
   }
-  results[1,i] = mean(abs(cor_theta))
-  results[2,i] = mean(pred_theta_sd)
-  results[3,i] = mean(pred_theta_ll)
-  results[4,i] = mean(train_lls)
-  results[5,i] = mean(train_acc)
-  results[6,i] = mean(pred_lls)
-  results[7,i] = mean(pred_acc)
-  results[8,i] = mean(abs(cor_icc))
-  results[9,i] = mean(rmse_icc)
+  results[1,i] = mean(abs(cor_theta), na.rm = TRUE)
+  results[2,i] = mean(pred_theta_sd[!is.infinite(pred_theta_sd)], na.rm = TRUE)
+  results[3,i] = mean(pred_theta_ll[!is.infinite(pred_theta_ll)], na.rm = TRUE)
+  results[4,i] = mean(train_lls, na.rm = TRUE)
+  results[5,i] = mean(train_acc, na.rm = TRUE)
+  results[6,i] = mean(pred_lls, na.rm = TRUE)
+  results[7,i] = mean(pred_acc, na.rm = TRUE)
+  results[8,i] = mean(abs(cor_icc), na.rm = TRUE)
+  results[9,i] = mean(rmse_icc, na.rm = TRUE)
 }
 
 write.csv(results, file=paste("./results/", HYP, ".csv" , sep=""))
