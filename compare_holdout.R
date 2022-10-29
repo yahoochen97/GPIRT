37eeb5ec6cde31b1b02663b48ef0745684e073f9
Yehu Chen <chenyehu@wustl.edu>
2022-10-29 12:31:59 -0500

    supremecourt holdout

--- compare_holdout.R
+++ compare_holdout.R
@@ -19,61 +19,46 @@ if (length(args)==5){
 MODELS = c("gpirt","doirt")
 horizon = TEST_YEAR - TRAIN_START_YEAR + 1
 
-results = data.frame(matrix(ncol = 5, nrow = 0))
-colnames(results) = c("type","measure","SEED","pvalue","diff_mean")
-# train 0/test h, acc, seed 1, gpirt, 0.5, E[gpirt]-E[doirt]
-
-# ttest_acc = matrix(0, nrow = MAXSEED, ncol = TEST_YEAR-TRAIN_END_YEAR+1)
-# ttest_lls = matrix(0, nrow = MAXSEED, ncol = TEST_YEAR-TRAIN_END_YEAR+1)
-# ttest_response = matrix(0, nrow = MAXSEED, ncol = TEST_YEAR-TRAIN_END_YEAR+1)
-# ttest_prediction = matrix(0, nrow = MAXSEED, ncol = TEST_YEAR-TRAIN_END_YEAR+1)
+ttest_acc = array(array(0, MAXSEED*(TEST_YEAR-TRAIN_END_YEAR+1)*length(MODELS)),
+                  c(MAXSEED,(TEST_YEAR-TRAIN_END_YEAR+1),length(MODELS)))
+ttest_lls = array(array(0, MAXSEED*(TEST_YEAR-TRAIN_END_YEAR+1)*length(MODELS)),
+                  c(MAXSEED,(TEST_YEAR-TRAIN_END_YEAR+1),length(MODELS)))
 
 for (SEED in 1:MAXSEED){
-  HYP = paste(MODELS[1], "_", DATANAME, "_holdout_SEED_", SEED , sep="")
-  load(file=paste("./results/", HYP, ".RData", sep=""))
-  train_acc1 = train_acc
-  train_lls1 = train_lls
-  train_response1 = train_response
-  train_prediction1 = train_prediction 
-  HYP = paste(MODELS[2], "_", DATANAME, "_holdout_SEED_", SEED , sep="")
-  load(file=paste("./results/", HYP, ".RData", sep=""))
-  train_acc2 = train_acc
-  train_lls2 = train_lls
-  train_response2 = train_response
-  train_prediction2 = train_prediction 
-  
-  tmp = t.test(train_acc1, train_acc2)
-  results[nrow(results)+1,] = c(0, "acc", SEED, tmp[["p.value"]],
-                     tmp[['estimate']][[1]]-tmp[['estimate']][[2]])
-  tmp = t.test(train_lls1, train_lls2)
-  results[nrow(results)+1,] = c(0, "ll", SEED, tmp[["p.value"]],
-                                tmp[['estimate']][[1]]-tmp[['estimate']][[2]])
-
-  for(h in 1:horizon) {
-    h_ = h+1999-TRAIN_END_YEAR
-    if(h_>0){
-      HYP = paste(MODELS[1], "_", DATANAME, "_holdout_SEED_", SEED , sep="")
-      load(file=paste("./results/", HYP, ".RData", sep=""))
-      test_acc1 = test_acc[[h_]]
-      test_lls1 = test_lls[[h_]]
-      test_response1 = test_response[[h_]]
-      test_prediction1 = test_prediction[[h_]]
-      
-      HYP = paste(MODELS[2], "_", DATANAME, "_holdout_SEED_", SEED , sep="")
-      load(file=paste("./results/", HYP, ".RData", sep=""))
-      test_acc2 = test_acc[[h_]]
-      test_lls2 = test_lls[[h_]]
-      test_response2 = test_response[[h_]]
-      test_prediction2 = test_prediction[[h_]]
-      
-      tmp = t.test(test_acc1, test_acc2)
-      results[nrow(results)+1,] = c(h_, "acc", SEED, tmp[["p.value"]],
-                                    tmp[['estimate']][[1]]-tmp[['estimate']][[2]])
-      tmp = t.test(test_lls1, test_lls2)
-      results[nrow(results)+1,] = c(h_, "ll", SEED, tmp[["p.value"]],
-                                    tmp[['estimate']][[1]]-tmp[['estimate']][[2]])
+  for(k in 1:length(MODELS)){
+    HYP = paste(MODELS[k], "_", DATANAME, "_holdout_SEED_", SEED , sep="")
+    load(file=paste("./results/", HYP, ".RData", sep=""))
+    ttest_acc[i,1,k] = mean(train_acc)
+    ttest_lls[i,1,k] = mean(train_lls)
+    
+    for(h in 1:horizon) {
+      h_ = h+1999-TRAIN_END_YEAR
+      if(h_>0){
+        ttest_acc[i,1+h_,k] = mean(test_acc[[h_]])
+        ttest_lls[i,1+h_,k] = mean(test_lls[[h_]])
+        
+      }
     }
   }
 }
+
+# t tests
+results = data.frame(matrix(ncol = 4, nrow = 0))
+colnames(results) = c("type","measure","pvalue","diff_mean")
+# train 0/test h, acc, pvalue0.5, E[gpirt]-E[doirt]
+
+
+for(h in 1:horizon) {
+  h_ = h+1999-TRAIN_END_YEAR
+  if(h_>=0){
+    tmp = t.test(ttest_acc[,h_+1,1], ttest_acc[,h_+1,2])
+    results[nrow(results)+1,] = c(h_, "acc", tmp[["p.value"]],
+                       tmp[['estimate']][[1]]-tmp[['estimate']][[2]])
+    tmp = t.test(ttest_lls[,h_+1,1], ttest_lls[,h_+1,2])
+    results[nrow(results)+1,] = c(h_, "acc", tmp[["p.value"]],
+                                  tmp[['estimate']][[1]]-tmp[['estimate']][[2]])
+  }
+}
+
 
 write.csv(results, paste("./results/",DATANAME,"_holdout.csv", sep=""))
