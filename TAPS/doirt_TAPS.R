a2057490ff89da5b262377280a0941aae18d6d37
Yehu Chen <chenyehu@wustl.edu>
2023-09-06 12:37:21 -0500

    TAPS

--- TAPS/doirt_TAPS.R
+++ TAPS/doirt_TAPS.R
@@ -3,6 +3,12 @@
 # gpirt_path = "~/Documents/Github/OrdGPIRT"
 # setwd(gpirt_path)
 # setwd("../TAPS")
+SAMPLE_ITERS = 10
+BURNOUT_ITERS = 10
+THIN = 4
+CHAIN = 1
+TYPE = "GP"
+
 print("Loading stan")
 
 R_path="~/R/x86_64-redhat-linux-gnu-library/4.0"
@@ -13,6 +19,22 @@ rstan_options(auto_write = TRUE)
 args = commandArgs(trailingOnly=TRUE)
 options(show.error.locations = TRUE)
 
+if (length(args)==0) {
+  TRAIN_START_YEAR = 1
+  TRAIN_END_YEAR = 31
+  TEST_YEAR = 41
+  DROP_RATIO = 5
+  SEED = 1
+}
+
+if (length(args)==5){
+  TRAIN_START_YEAR = as.integer(args[1])
+  TRAIN_END_YEAR = as.integer(args[2])
+  TEST_YEAR = as.integer(args[3])
+  SEED = as.integer(args[4])
+  DROP_RATIO = as.integer(args[5])
+}
+
 library(gpirt)
 library(dplyr)
 library(ggplot2)
@@ -21,22 +43,29 @@ library(ltm)
 
 source("load_TAPS.R")
 
+gpirt_data_train = gpirt_data
+
+# set random seed
+set.seed(SEED)
+
+# random drop one observation for every case from TRAIN_END_YEAR+1 to TEST_YEAR
+for(h in (TRAIN_END_YEAR+1):(TEST_YEAR)){
+  for (j in 1:m){
+    mask = !is.na(gpirt_data[,j,h])
+    drop_unit = sample(which(mask==1), DROP_RATIO/100*length(mask))
+    gpirt_data_train[drop_unit, j, h] = NA
+  }
+}
+
 # code na as 0 for stan to ignore
-na_mask = is.na(gpirt_data)
-gpirt_data[na_mask] = 0
+gpirt_data_train[is.na(gpirt_data_train)] = 0
 
-SAMPLE_ITERS = 500
-BURNOUT_ITERS = 500
-SEED = 12345
-
-THIN = 4
-CHAIN = 3
 stan_data <- list(n=n,
                   m=m,
                   horizon=horizon,
                   K=C,
                   sigma=0.1,
-                  y=gpirt_data)
+                  y=gpirt_data_train)
 
 # train stan model
 fit <- stan(file = "../doirt-synthetic.stan",
@@ -51,7 +80,7 @@ fit <- stan(file = "../doirt-synthetic.stan",
             refresh=1
 )
 
-save.image(file='doirt_TAPS_2014.RData')
+# save.image(file='doirt_TAPS_2014.RData')
 
 gpirt_data[na_mask] = NA
 
@@ -121,8 +150,22 @@ gpirt_iccs = array(array(0, length(xs)*m*horizon),
 
 C=5
 
+# train/test statistics
+# train
+train_lls = c()
+train_acc = c()
+train_response = c()
+train_prediction = c()
+
+# test
+test_lls = vector(mode='list', length=(TEST_YEAR-TRAIN_END_YEAR))
+test_acc = vector(mode='list', length=(TEST_YEAR-TRAIN_END_YEAR))
+test_response = vector(mode='list', length=(TEST_YEAR-TRAIN_END_YEAR))
+test_prediction = vector(mode='list', length=(TEST_YEAR-TRAIN_END_YEAR))
+
 for (h in 1:horizon) {
   wave = unique(data$wave)[h]
+  h_ = h-TRAIN_END_YEAR
   for (j in 1:m) {
     IRFs = matrix(0, nrow=SAMPLE_ITERS, ncol=length(xs))
     mask = rep(0, SAMPLE_ITERS)
@@ -146,9 +189,39 @@ for (h in 1:horizon) {
       group_by(xs) %>%
       summarize(icc=sum(order*p))
     gpirt_iccs[,j,h] = tmp$icc
+    
+    # test/train statistic
+    for (i in 1:n) {
+      if(!is.na(gpirt_data_train[i,j,h]) & !is.na(gpirt_data[i,j,h])){
+        # train
+        pred_idx = 1+as.integer((pred_theta[i,h]+5)*100)
+        ll = log(probs$p[probs$xs==xs[pred_idx]])
+        y_pred = which.max(ll)
+        train_acc = c(train_acc, y_pred==(gpirt_data[i,j, h]))
+        train_lls = c(train_lls, ll[gpirt_data[i,j, h]])
+        train_response = c(train_response, gpirt_data[i,j, h])
+        train_prediction = c(train_prediction, y_pred)
+      }
+      if(is.na(gpirt_data_train[i,j,h]) & !is.na(gpirt_data[i,j,h])){
+        # test
+        pred_idx = 1+as.integer((pred_theta[i,h]+5)*100)
+        ll = log(probs$p[probs$xs==xs[pred_idx]])
+        y_pred = which.max(ll)
+        test_acc[[h_]] = c(test_acc[[h_]], y_pred==(gpirt_data[i,j,h]))
+        test_lls[[h_]] = c(test_lls[[h_]], ll[gpirt_data[i,j, h]])
+        test_response[[h_]] = c(test_response[[h_]],gpirt_data[i,j, h])
+        test_prediction[[h_]] = c(test_prediction[[h_]], y_pred)
+      }
+    }
   }
 }
 
 print("doirt finished!")
 
-save.image(file='doirt_TAPS_2014.RData')
+save(gpirt_data_train, gpirt_data, pred_theta,pred_theta_sd,train_lls,
+     train_acc, train_response, train_prediction,test_lls,
+     test_acc, test_response, test_prediction,
+     file=paste("./results/doirt_TAPS_holdout_", "DR_", DROP_RATIO, "_SEED_", SEED, ".RData" , sep=""))
+
+
+# save.image(file='doirt_TAPS_2014.RData')
